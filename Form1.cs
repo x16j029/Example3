1e979ecec898935f8e67b4595420ed557effe7f1
WIN_NT\x16j029 <x16j029@chiba-fjb.ac.jp>
2017-04-28 12:32:42 +0900

    llllllllllll

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace Example3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = String.Format("{0}", 1 + 2 * 3);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Text = String.Format("{0}", (1 + 2) * 3);
+        }
     }
 }
